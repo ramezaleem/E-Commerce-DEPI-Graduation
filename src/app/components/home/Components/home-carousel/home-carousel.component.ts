7b6fea7ebadda7c8bf1a34d7adfc409b87232dec
heka <Aee23530@gmail.com>
2024-10-14 01:59:28 +0300

    home carousel .

--- src/app/components/home/Components/home-carousel/home-carousel.component.ts
+++ src/app/components/home/Components/home-carousel/home-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-home-carousel',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './home-carousel.component.scss'
 })
 export class HomeCarouselComponent {
+  heroImage:string ='src\app\components\home\Components\home-carousel\images\hero.jpg';
+  @ViewChildren ('icon' ) icoon! : QueryList<ElementRef>;
+  print(){
+    console.log(this.icoon);
+    this.icoon.forEach(icon=>{
+      icon.nativeElement.style.color= 'red'
+    })
+  }
+
 
 }
