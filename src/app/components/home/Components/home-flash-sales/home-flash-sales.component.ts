45e3eeed024cb85416bf26a9c1419e0140774797
ramezaleem <ramez.aleem532@gmail.com>
2024-10-16 21:08:40 +0300

    Finsh Flash-sales and Best-Selling Design

--- src/app/components/home/Components/home-flash-sales/home-flash-sales.component.ts
+++ src/app/components/home/Components/home-flash-sales/home-flash-sales.component.ts
@@ -5,6 +5,7 @@ import {
   OnInit,
   OnDestroy,
 } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home-flash-sales',
@@ -12,20 +13,132 @@ import {
   styleUrls: ['./home-flash-sales.component.scss'],
 })
 export class HomeFlashSalesComponent implements OnInit, OnDestroy {
+  @Output() navigateCarousel: EventEmitter<string> = new EventEmitter();
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    items: 4.5,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+      1200: {
+        items: 4.5,
+      },
+    },
+    nav: true,
+  };
+
   timerVisible: boolean = true;
   countdown: any;
+
   days: number = 0;
   hours: number = 0;
   minutes: number = 0;
   seconds: number = 0;
 
-  @Output() navigateCarousel: EventEmitter<string> = new EventEmitter();
-
-  // Product data
+  // Product Data
+  // products = [
+  //   {
+  //     id: 1,
+  //     name: 'HAVIT HV-G92 Gamepad',
+  //     image: 'assets/images/PlayStation.png',
+  //     discount: '-40%',
+  //     price: 299.99,
+  //     originalPrice: 499.99,
+  //     rating: 4.5,
+  //     numberOfRatings: 150,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Ak-900 Wired Keyboard',
+  //     image: 'assets/images/Wired-Keyboard.jpeg',
+  //     discount: '-35%',
+  //     price: 49.99,
+  //     originalPrice: 79.99,
+  //     rating: 5.0,
+  //     numberOfRatings: 200,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Ips LCD Gaming Monitor',
+  //     image: 'assets/images/Gaming-Monitor.jpg',
+  //     discount: '-25%',
+  //     price: 29.99,
+  //     originalPrice: 39.99,
+  //     rating: 4.0,
+  //     numberOfRatings: 150,
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'S-Series Comfor Chair',
+  //     image: 'assets/images/Chair.jpeg',
+  //     discount: '-15%',
+  //     price: 199.99,
+  //     originalPrice: 249.99,
+  //     rating: 4.8,
+  //     numberOfRatings: 300,
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Logitech G Pro X Gaming Headset',
+  //     image: 'assets/images/Gaming-Headset.jpg',
+  //     discount: '-30%',
+  //     price: 139.99,
+  //     originalPrice: 199.99,
+  //     rating: 4.7,
+  //     numberOfRatings: 250,
+  //   },
+  //   {
+  //     id: 6,
+  //     name: 'Razer BlackWidow V3 Keyboard',
+  //     image: 'assets/images/Razer-Keyboard.jpg',
+  //     discount: '-20%',
+  //     price: 79.99,
+  //     originalPrice: 99.99,
+  //     rating: 4.6,
+  //     numberOfRatings: 180,
+  //   },
+  //   {
+  //     id: 7,
+  //     name: 'ASUS 27" Gaming Monitor',
+  //     image: 'assets/images/Gaming-Monitor.jpg',
+  //     discount: '-15%',
+  //     price: 339.99,
+  //     originalPrice: 399.99,
+  //     rating: 4.5,
+  //     numberOfRatings: 210,
+  //   },
+  //   {
+  //     id: 8,
+  //     name: 'DXRacer Racing Series Gaming Chair',
+  //     image: 'assets/images/Gaming-Chair.jpg',
+  //     discount: '-25%',
+  //     price: 249.99,
+  //     originalPrice: 329.99,
+  //     rating: 4.8,
+  //     numberOfRatings: 320,
+  //   },
+  // ];
   products = [
     {
       id: 1,
-      name: 'Product 1',
+      name: 'HAVIT HV-G92 Gamepad',
       image: 'assets/images/PlayStation.png',
       discount: '-40%',
       price: 299.99,
@@ -45,23 +158,63 @@ export class HomeFlashSalesComponent implements OnInit, OnDestroy {
     },
     {
       id: 3,
-      name: 'Wireless Mouse',
-      image: 'assets/images/Wired-Keyboard.jpeg',
+      name: 'Ips LCD Gaming Monitor',
+      image: 'assets/images/Gaming-Monitor.jpg',
       discount: '-25%',
-      price: 29.99,
-      originalPrice: 39.99,
+      price: 189.99, // Adjusted to a more realistic price
+      originalPrice: 249.99,
       rating: 4.0,
       numberOfRatings: 150,
     },
     {
       id: 4,
-      name: 'Gaming Monitor',
-      image: 'assets/images/Wired-Keyboard.jpeg',
+      name: 'S-Series Comfort Chair',
+      image: 'assets/images/Chair.jpeg',
       discount: '-15%',
-      price: 199.99,
+      price: 212.49, // Adjusted to a more realistic price
       originalPrice: 249.99,
       rating: 4.8,
       numberOfRatings: 300,
+    },
+    {
+      id: 5,
+      name: 'Logitech Gaming Headset',
+      image: 'assets/images/Headset-Gaming.jpeg',
+      discount: '-30%',
+      price: 139.99,
+      originalPrice: 199.99,
+      rating: 4.7,
+      numberOfRatings: 250,
+    },
+    {
+      id: 6,
+      name: 'Razer BlackWidow Keyboard',
+      image: 'assets/images/Razer-Keyboard.jpeg',
+      discount: '-20%',
+      price: 79.99,
+      originalPrice: 99.99,
+      rating: 4.6,
+      numberOfRatings: 180,
+    },
+    {
+      id: 7,
+      name: 'ASUS 27" Gaming Monitor',
+      image: 'assets/images/ASUS-Gaming-Monitor.jpeg',
+      discount: '-15%',
+      price: 339.99,
+      originalPrice: 399.99,
+      rating: 4.5,
+      numberOfRatings: 210,
+    },
+    {
+      id: 8,
+      name: 'DXRacer Gaming Chair',
+      image: 'assets/images/Gaming-Chair.jpeg',
+      discount: '-25%',
+      price: 249.99,
+      originalPrice: 329.99,
+      rating: 4.8,
+      numberOfRatings: 320,
     },
   ];
 
@@ -95,7 +248,6 @@ export class HomeFlashSalesComponent implements OnInit, OnDestroy {
     clearInterval(this.countdown);
   }
 
-  // Emit navigation events
   onNavigate(direction: string) {
     this.navigateCarousel.emit(direction);
   }
