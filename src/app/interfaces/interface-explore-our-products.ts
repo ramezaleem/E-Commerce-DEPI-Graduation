ac166a4f5b326990605258f22f535fccd6bf5eda
ramezaleem <ramez.aleem532@gmail.com>
2024-10-19 21:01:41 +0300

    Add Interface And Service For Explore-Our-Products

---
+++ src/app/interfaces/interface-explore-our-products.ts
@@ -0,0 +1,10 @@
+export interface IExploreOurProducts {
+  id: number;
+  name: string;
+  image: string;
+  discount: string;
+  price: number;
+  originalPrice: string | null;
+  rating: number;
+  numberOfRatings: number;
+}
---
+++ src/app/services/explore-our-products.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { IExploreOurProducts } from '../interfaces/interface-explore-our-products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExploreOurProductsService {
+  constructor() {}
+
+  Products: IExploreOurProducts[] = [
+    {
+      id: 1,
+      name: 'Dry Dog Food',
+      image: 'assets/images/ex1.png',
+      discount: '-40%',
+      price: 260,
+      originalPrice: null, // Set to null
+      rating: 4.5,
+      numberOfRatings: 150,
+    },
+    {
+      id: 2,
+      name: 'Canon Camera',
+      image: 'assets/images/ex2.png',
+      discount: '-35%',
+      price: 960,
+      originalPrice: null, // Set to null
+      rating: 5.0,
+      numberOfRatings: 200,
+    },
+    {
+      id: 3,
+      name: 'ASUS Gaming Laptop',
+      image: 'assets/images/ex3.png',
+      discount: '-25%',
+      price: 160,
+      originalPrice: null, // Set to null
+      rating: 4.0,
+      numberOfRatings: 150,
+    },
+    {
+      id: 4,
+      name: 'Product Set',
+      image: 'assets/images/ex4.png',
+      discount: '-15%',
+      price: 360,
+      originalPrice: null, // Set to null
+      rating: 4.8,
+      numberOfRatings: 300,
+    },
+    {
+      id: 5,
+      name: 'Kids Electric Car',
+      image: 'assets/images/ex5.png',
+      discount: '-15%',
+      price: 360,
+      originalPrice: null, // Set to null
+      rating: 4.8,
+      numberOfRatings: 300,
+    },
+    {
+      id: 6,
+      name: 'Zoom Soccer Cleats',
+      image: 'assets/images/ex6.png',
+      discount: '-15%',
+      price: 360,
+      originalPrice: null, // Set to null
+      rating: 4.8,
+      numberOfRatings: 300,
+    },
+    {
+      id: 7,
+      name: 'Shooter USB Gamepad',
+      image: 'assets/images/ex7.png',
+      discount: '-15%',
+      price: 360,
+      originalPrice: null, // Set to null
+      rating: 4.8,
+      numberOfRatings: 300,
+    },
+    {
+      id: 8,
+      name: 'Quilted Satin Jacket',
+      image: 'assets/images/ex6 (2).png',
+      discount: '-15%',
+      price: 360,
+      originalPrice: null, // Set to null
+      rating: 4.8,
+      numberOfRatings: 300,
+    },
+  ];
+}
