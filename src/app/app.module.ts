3dfdd97c62284774c66fc016a33002c2ad0c8852
ramezaleem <ramez.aleem532@gmail.com>
2024-10-19 21:31:08 +0300

    No Thing At This Time, Waiting To Find Error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { RouterModule } from '@angular/router'; // Import RouterModule
 
 import { HomeComponent } from './components/home/home.component';
 import { HomeCarouselComponent } from './components/home/Components/home-carousel/home-carousel.component';
@@ -48,12 +49,12 @@ import { SecondExploreOurProductsComponent } from './components/home/Components/
     AboutMidComponent,
     AboutFootComponent,
     SecondExploreOurProductsComponent,
-     SecondProductComponentComponent,
+    SecondProductComponentComponent,
     ContactComponent,
     AllProductsComponent,
     NewArrivalComponent,
     Error404Component,
-    SecondProductComponentComponent
+    SecondProductComponentComponent,
   ],
   imports: [
     BrowserModule,
